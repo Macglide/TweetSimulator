c8089fac3bcfaf679ee2239a96367ea19cf16a97
Ofentse Sebidi <ofentse.sebidi@rank.com>
2022-10-03 22:51:58 +0200

    Add Unit test

---
+++ src/test/java/za/co/mycompany/twittersimulater/web/rest/TwitterResourceTest.java
@@ -0,0 +1,52 @@
+package za.co.mycompany.twittersimulater.web.rest;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import za.co.mycompany.twittersimulater.service.FileProcessingService;
+
+public class TwitterResourceTest {
+
+    @Mock
+    private FileProcessingService fileProcessingService;
+
+    @InjectMocks
+    private TwitterResource twitterResource;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test //success
+    public void givenUserAndTweetFiles_whenUpdateTwitterFeed_thenPrintTweetsToConsole() {
+        //given
+        MockMultipartFile userfile = new MockMultipartFile(
+            "userfile",
+            "userfile.txt",
+            MediaType.TEXT_PLAIN_VALUE,
+            "<<text/plain>>".getBytes(StandardCharsets.UTF_8)
+        );
+
+        MockMultipartFile tweetfile = new MockMultipartFile(
+            "userfile",
+            "userfile.txt",
+            MediaType.TEXT_PLAIN_VALUE,
+            "<<text/plain>>".getBytes(StandardCharsets.UTF_8)
+        );
+
+        //when
+        twitterResource.updateTwitterFeed(userfile, tweetfile);
+
+        //then
+        verify(fileProcessingService, times(1)).printUsersAndTweetsToConsole();
+    }
+}
